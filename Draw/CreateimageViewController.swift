de6b3e3e6bc29337301b7665db90d1ac68df7500
Burenkovuser <nb2207@yandex.ru>
2016-10-25 14:45:30 +0700

    added gradient layer with clipping

--- Draw/CreateimageViewController.swift
+++ Draw/CreateimageViewController.swift
@@ -31,13 +31,13 @@ class CreateimageViewController: UIViewController {
         
         drawGradientOfSize(imadeView.frame.size, context: context)
         
+        drawRadialGradientOfSize(imadeView.frame.size, context: context)
+        
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         return image
-        
-        
     }
     
     
@@ -60,6 +60,32 @@ class CreateimageViewController: UIViewController {
         let gradient = CGGradientCreateWithColors(colorSpace, colors, positions)
         
         CGContextDrawLinearGradient(context, gradient, startPoint, finishPoint, [])
+        
+    }
+    
+    func drawRadialGradientOfSize(size: CGSize, context: CGContextRef) {
+        
+        let clipPath = UIBezierPath(roundedRect: CGRectMake(40, 40, size.width/2, size.width/1.5), byRoundingCorners: [.BottomLeft, .TopRight], cornerRadii: CGSizeMake(10, 10))
+        
+        clipPath.addClip()
+        
+        let colors = [
+            UIColor.blackColor().CGColor,
+            UIColor.whiteColor().CGColor,
+            UIColor.purpleColor().CGColor,
+            UIColor.greenColor().CGColor
+        ]
+        
+        let positions:[CGFloat]   = [0.2, 0.4, 0.6, 0.9]
+        let startPoint  = CGPointMake(10, 10)
+        let finishPoint = CGPointMake(size.width, size.height)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        
+        let gradient = CGGradientCreateWithColors(colorSpace, colors, positions)
+        
+        CGContextDrawRadialGradient(context, gradient, startPoint, 10, finishPoint, 100, [])
         
     }
     
